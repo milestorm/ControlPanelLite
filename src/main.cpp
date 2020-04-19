f6d12abdd6f0fb5f8b551b1803f4c3339e582106
Caivas, David <David.Caivas@firma.seznam.cz>
2020-04-19 20:44:55 +0200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,17 @@
 #include <flasher.h>
 #include <OneButton.h>
 #include <tools.h>
+#include <MaxMatrix.h>
+
+MaxMatrix dotMatrix(MATRIX_DIN, MATRIX_CS, MATRIX_CLK, 1);
 
 OneButton myButtons[] = {OneButton(BUTTON_R, true), OneButton(BUTTON_G, true), OneButton(BUTTON_B, true), OneButton(BUTTON_Y, true)};
 Flasher myLeds[] = {Flasher(LIGHT_R, 300, 300), Flasher(LIGHT_G, 300, 300), Flasher(LIGHT_B, 300, 300), Flasher(LIGHT_Y, 300, 300)};
+
+byte xicht_happy[] =   {8, 8, B00000000, B00100000, B01001100, B01000000, B01000000, B01001100, B00100000, B00000000};
+byte xicht_wink[] =    {8, 8, B00000000, B00100100, B01000100, B01000000, B01000000, B01001100, B00100000, B00000000};
+byte xicht_sad[] =     {8, 8, B00000000, B01000000, B00101100, B00100000, B00100000, B00101100, B01000000, B00000000};
+byte xicht_default[] = {8, 8, B00000000, B00000000, B00101100, B00100000, B00100000, B00101100, B00000000, B00000000};
 
 int activeLed = -1;
 
@@ -63,10 +71,13 @@ void butt3Click() {
 // -----------------------------------------
 
 void setup() {
+  dotMatrix.init(); // module initialize
+  dotMatrix.setIntensity(3); // dot matix intensity 0-15
   // bootup
-  tone(SPEAKER, 880, 100);
   turnOnAllLeds();
-  delay(500);
+  delay(1000);
+  dotMatrix.writeSprite(0, 0, xicht_happy);
+  tone(SPEAKER, 880, 100);
   turnOffAllLeds();
 
   // Button game
